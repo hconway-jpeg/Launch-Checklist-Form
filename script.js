c7216032d0a03ffe1b7c00563c7a3ed91fe71ac6
Hannah <hconway@luc.edu>
2020-12-02 12:57:35 -0600

    updated validations

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
 // Write your JavaScript code here!
 window.addEventListener("load", function() {
-   fetch("").then(function(response) {
-      response.json(function(json) {
-         const missionTarget = document.getElementById("missionTarget");
-      });
+   let form = document.querySelector("form");
+   form.addEventListener("submit", function(event) {
+      let pilotNameInput = document.querySelector("input[name=pilotName]");
+      let coPilotNameInput = document.querySelector("input[name=copilotName]");
+      let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
+      let cargoMassInput = document.querySelector("input[name=cargoMass]");
+         if (pilotNameInput.value === "" || coPilotNameInput.value === "" || fuelLevelInput.value === "" || cargoMassInput.value === "") {
+            alert("All fields are required");
+            event.preventDefault();
+         }
+         if (isNaN(pilotNameInput.value) === false || isNaN(coPilotNameInput.value) === false || isNaN(fuelLevelInput.value) === true || isNaN(cargoMassInput.value) === true) {
+            alert("Invalid entry");
+            event.preventDefault();
+         }
    });
+   // fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
+   //    response.json(function(json) {
+   //       const missionTarget = document.getElementById("missionTarget");
+   //       missionTarget.innerHTML += `
+   //          <h2>Mission Destination</h2>
+   //          <ol>
+   //             <li>Name: ${}</li>
+   //             <li>Diameter: ${}</li>
+   //             <li>Star: ${}</li>
+   //             <li>Distance from Earth: ${}</li>
+   //             <li>Number of Moons: ${}</li>
+   //          </ol>
+   //          <img src="${}">
+   //          `;
+   //    });
+   // });
 });
-
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
