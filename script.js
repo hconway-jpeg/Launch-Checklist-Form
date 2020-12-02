f741f431ed4177db56b308832f592397dca7be1c
Conway <hconway@luc.edu>
2020-12-02 08:00:38 -0600

    first commit for forms assignment

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 // Write your JavaScript code here!
+window.addEventListener("load", function() {
+   fetch("").then(function(response) {
+      response.json(function(json) {
+         const missionTarget = document.getElementById("missionTarget");
+      });
+   });
+});
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
