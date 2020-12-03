89e83ccc3a0371e29e1cb5fc077600c7cdcd8605
Hannah <hconway@luc.edu>
2020-12-03 11:00:03 -0600

    working on fetch, but still some errors

--- script.js
+++ script.js
@@ -7,38 +7,47 @@ window.addEventListener("load", function() {
       let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
       let cargoMassInput = document.querySelector("input[name=cargoMass]");
       let faultyItems = document.getElementById("faultyItems");
-         
+      let launchStatus = document.getElementById("launchStatus");
+      let fuelStatus = document.getElementById("fuelStatus");
+      let cargoStatus = document.getElementById("cargoStatus");
+
       if (pilotNameInput.value === "" || coPilotNameInput.value === "" || fuelLevelInput.value === "" || cargoMassInput.value === "") {
          alert("All fields are required");
          event.preventDefault();
       } else if (isNaN(pilotNameInput.value) === false || isNaN(coPilotNameInput.value) === false || isNaN(fuelLevelInput.value) === true || isNaN(cargoMassInput.value) === true) {
          alert("Invalid entry");
          event.preventDefault();
-      }  else if (fuelLevelInput.value < 10000) {
+      } else if (fuelLevelInput.value < 10000) {
+         launchStatus.innerHTML = "Shuttle not ready for launch";
+         launchStatus.style.color = "red";
          faultyItems.style.visibility = "visible";
-         faultyItems.style.visibility = "inherit";
+         fuelStatus.innerHTML = "Fuel level too low for launch";
+      } else if (cargoMassInput.value > 10000) {
+         launchStatus.innerHTML = "Shuttle not ready for launch";
+         launchStatus.style.color = "red";
+         faultyItems.style.visibility = "visible";
+         cargoStatus = "Cargo mass too heavy for launch";
+      } else {
+         launchStatus.innerHTML = "Shuttle is ready for launch";
+         launchStatus.style.color = "green";
+         // fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
+         //    response.json(function(json) {
+         //       const missionTarget = document.getElementById("missionTarget");
+         //          for (planets of json) {
+         //             missionTarget.innerHTML += `
+         //                <h2>Mission Destination</h2>
+         //                <ol>
+         //                   <li>Name: ${planets.name}</li>
+         //                   <li>Diameter: ${}</li>
+         //                   <li>Star: ${}</li>
+         //                   <li>Distance from Earth: ${}</li>
+         //                   <li>Number of Moons: ${}</li>
+         //                </ol>
+         //                <img src="${}">
+         //             `;
+         //          }
+         //    });
+         // });
       }
-         
    });
 });
-
-
-
-
-
-   // fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
-   //    response.json(function(json) {
-   //       const missionTarget = document.getElementById("missionTarget");
-   //       missionTarget.innerHTML += `
-   //          <h2>Mission Destination</h2>
-   //          <ol>
-   //             <li>Name: ${}</li>
-   //             <li>Diameter: ${}</li>
-   //             <li>Star: ${}</li>
-   //             <li>Distance from Earth: ${}</li>
-   //             <li>Number of Moons: ${}</li>
-   //          </ol>
-   //          <img src="${}">
-   //          `;
-   //    });
-   // });
